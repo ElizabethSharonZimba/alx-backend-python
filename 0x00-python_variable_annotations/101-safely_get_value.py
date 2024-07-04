cd4c693db6859b1ef2ac22ef21e8b656745bfcee
ElizabethSharonZimba <sharonzimba95@gmail.com>
2024-07-04 18:45:16 +0000

    py

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python3
 """
-    Duck typing Typevar
+Duck typing with TypeVar
 """
 from typing import Mapping, TypeVar, Any, Union
+
 T = TypeVar('T')
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None)\
-        -> Union[Any, T]:
+
+def safely_get_value(dct: Mapping[Any, T], key: Any, default: Union[T, None] = None) -> Union[T, None]:
     """
-        Args:
-            dct: Mapping
-            key: Any data type
-            default: Default value
-        Return:
-            Any or T format
+    Safely get value from a dictionary with a default.
+
+    Args:
+        dct: A dictionary-like mapping.
+        key: The key to lookup.
+        default: The default value to return if the key is not found.
+
+    Returns:
+        The value associated with the key if it exists, otherwise the default value.
     """
     if key in dct:
         return dct[key]
     else:
         return default
+
--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python3
 """
-    Type checking
+Type checking
 """
 from typing import Tuple, List
-def zoom_array(lst: Tuple, factor: int = 2) -> List:
+
+def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
     """
-        Args:
-            lst: Tuple of numbers
-            factor: increase factor
-        Return:
-            zoomed
+    Args:
+        lst: Tuple of numbers
+        factor: increase factor
+    Return:
+        zoomed
     """
-    zoomed_in: List = [
+    zoomed_in: List[int] = [
         item for item in lst
         for i in range(factor)
     ]
     return zoomed_in
+
 array = (12, 72, 91)
+
 zoom_2x = zoom_array(array)
-zoom_3x = zoom_array(array, 3)
+zoom_3x = zoom_array(array,
+
